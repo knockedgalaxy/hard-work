21f98c72927e24049beb75a202d11dd290bdfb13
mi <lotsoff@mail.ru>
2023-11-07 14:22:10 +0300

    add and delete

--- vowels.py
+++ vowels.py
@@ -1,6 +1,6 @@
-vowels = ['a','e','i','o','u']
+vowels = ['o','e']
 found=[]
-word = input('provide a word to search for wovels:')
+word = input('Provide a word to search for wovels:')
 for letter in word:
     if letter in vowels:
        if letter not in found:
